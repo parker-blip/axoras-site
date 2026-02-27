449e5085f96bd7119185bf810631ebba5fd78da6
parker-blip <parker@devisser.ca>
2026-02-26 19:54:25 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
+import Features from './pages/Features.tsx'
 import ClientDetail from './pages/ClientDetail.tsx'
 import Admin from './pages/Admin.tsx'
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
         <Route path="/client/:clientId" element={<ClientDetail />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
